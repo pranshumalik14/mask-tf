e09e8ce23b3899c6edd3818630b4a28bb183cbc9
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2021-10-13 17:16:08 -0400

    Updated recording script, working for 2 microphones

--- record.py
+++ record.py
@@ -1,54 +1,83 @@
-# class PyAudio:
-#     close
-#     get_default_input_device_info
-#     get_device_count
-#     get_device_info_by_index
-#     open
-#     terminate
-
-# Stream.__init__
-
-
+# Records and saves audio from one or more microphones
 # Based on https://gist.github.com/mabdrabo/8678538
 
+# Example run command: python3 record.py -d 4 2 -f 400hz
 
+# Need to find audio device index/indices using the info.py script before
+# running this script
+# If the script produces the error "OSError: [Errno -9981] Input overflowed",
+# try reversing the order of the device index command line arguments to fix the
+# error
 
+import argparse
 import pyaudio
 import wave
- 
+
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 48000
-CHUNK = 1024
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "file.wav"
+CHUNK = 1024    # Number of audio samples in buffer
+
+# Get command line input arguments
+parser = argparse.ArgumentParser(description=("Audio recording"))
+# nargs specifies the number of arguments, with "+" inserting arguments of that
+# type into a list
+parser.add_argument("-d", "--device", required=True, nargs="+", metavar=("device"),
+    help="Device index/indices for microphone(s) to record from")
+parser.add_argument("-f", "--file", required=True, metavar=("file"),
+    help="File name prefix for recording files")
+
+args = parser.parse_args()
+device_indices = list(map(int, args.device))   # This is a list of integers
+num_devices = len(device_indices)
+file_prefix = args.file
 
 audio = pyaudio.PyAudio()
- 
-# start Recording
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                # input_device_index = INDEX,
-                frames_per_buffer=CHUNK)
-print ("recording...")
-frames = []
- 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-print ("finished recording")
- 
- 
-# stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
- 
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
 
-print("Finished script")
+# Start recording for each microphone
+streams = []
+for i in range(num_devices):
+    device_index = device_indices[i]    
+    # See Stream.__init__() for argument descriptions
+    streams.append(audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    input_device_index=device_index,
+                    frames_per_buffer=CHUNK))
+    print(f"Opened stream for device index {device_index}")
+print("Recording...")
+
+# Save audio samples while recording
+frames = [[] for i in range(num_devices)]
+print("Press Ctrl-C at any time to stop recording")
+try:
+    while True:
+        for i in range(num_devices):
+            data = streams[i].read(CHUNK)
+            frames[i].append(data)
+except KeyboardInterrupt:
+    print("Finished recording")
+
+# Stop recording
+for i in range(num_devices):
+    device_index = device_indices[i] 
+    streams[i].stop_stream()
+    streams[i].close()
+    print(f"Closed stream for device index {device_index}")
+
+# Write audio to files
+for i in range(num_devices):
+    device_index = device_indices[i]
+
+    info = audio.get_device_info_by_index(device_index)
+    device_name = info["name"].replace(" ", "") # Remove spaces for file name
+    file_name = file_prefix + "-" + device_name + ".wav"
+
+    wave_file = wave.open(file_name, "wb")
+    wave_file.setnchannels(CHANNELS)
+    wave_file.setsampwidth(audio.get_sample_size(FORMAT))
+    wave_file.setframerate(RATE)
+    wave_file.writeframes(b"".join(frames[i]))
+    wave_file.close()
+    print(f"Wrote recording for device index {device_index} to file {file_name}")
+
+audio.terminate()
