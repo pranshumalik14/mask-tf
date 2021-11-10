080efcb0156657839d7bd35639d34d8b0c83b8e8
Pranshu Malik <pranshumalik14@gmail.com>
2021-11-10 12:06:47 -0500

    mask tf verified to be working!

--- scripts/mask_tf.m
+++ scripts/mask_tf.m
@@ -12,28 +12,7 @@ nbins = 1000;
 % get calibration tf from mic 1 (input) to match mic 2 (output) recording
 [tf_calib, fr_bins] = get_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins);
 
-% apply tf to mic 1 FFT
-M_1 = fftshift(fft(mic_1));
-M_2 = fftshift(fft(mic_2));
-N_1 = length(M_1);
-N_2 = length(M_2);
-df_1  = Fs_1/N_1;
-df_2  = Fs_2/N_2;
-fr_1  = -Fs_1/2:df_1:Fs_1/2-df_1;
-fr_2  = -Fs_2/2:df_2:Fs_2/2-df_2;
-
-idx_1 = fr_1(-fmax < fr_1 & fr_1 < fmax); idx_1 = round([(idx_1(1)-fr_1(1))/df_1 (idx_1(end)-fr_1(1))/df_1]);
-idx_2 = fr_2(-fmax < fr_2 & fr_2 < fmax); idx_2 = round([(idx_2(1)-fr_2(1))/df_2 (idx_2(end)-fr_2(1))/df_2]);
-
-for k = idx_1(1):idx_1(2)
-    M_1(k) = apply_tf(fr_1(k), M_1(k), tf_calib, fmax, nbins);
-end
-
-% truncate FFTs after application of tf for (close-to) identical reconstruction within fmax
-M_1(1:idx_1(1)-1) = 0;
-M_1(idx_1(2)+1:end) = 0;
-M_2(1:idx_2(1)-1) = 0;
-M_2(idx_2(2)+1:end) = 0;
+[mic_1_calib, mic_2_calib] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib);
 
 %% estimating mask tf
 % step 1: get mask tf mic recordings
@@ -58,16 +37,53 @@ M_2(idx_2(2)+1:end) = 0;
 [tf_mask_3, ~] = get_mask_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib);
 
 % average tf over all recordings
-tf_mask_avg = (tf_mask_1 + tf_mask_2 + tf_mask_3)/3;
+tf_mask_avg = smooth((tf_mask_1 + tf_mask_2 + tf_mask_3)/3);
 
 % plot tf avg
 figure;
 stairs(fr_bins, tf_mask_avg);
 title('Estimated Avg TF');
 
+%% apply tf to a non-masked recording
+
+% todo: here we should compare speech and see if we get a similar response
+[mic_1, Fs_1]  = audioread('../recordings/mask_tf_estimate/mask_tf_1_1_trimmed.wav');
+[mic_2, Fs_2]  = audioread('../recordings/mask_tf_estimate/mask_tf_1_2_trimmed.m4a');
+[mic_1, mic_2] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib);
+[mic_1, mic_2] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_mask_avg);
+
+% check if ffts of mic_1 and mic_2 match each other
+M_1 = fftshift(fft(mic_1));
+M_2 = fftshift(fft(mic_2));
 figure;
-stairs(fr_bins, smooth(tf_mask_avg));
-title('Smoothened Avg TF');
+plot(abs(M_1));
+figure;
+plot(abs(M_2));
+
+%% invert tf to get un-masked recording
+
+% todo: here we should compare speech and see if we get a similar response
+[mic_1, Fs_1]  = audioread('../recordings/mask_tf_estimate/mask_tf_1_1_trimmed.wav');
+[mic_2, Fs_2]  = audioread('../recordings/mask_tf_estimate/mask_tf_1_2_trimmed.m4a');
+[mic_1, mic_2] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib);
+[mic_2, mic_1] = calib_mics(mic_2, mic_1, Fs_2, Fs_1, fmax, nbins, 1./tf_mask_avg);
+
+% check if ffts of mic_1 and mic_2 match each other
+M_1 = fftshift(fft(mic_1));
+M_2 = fftshift(fft(mic_2));
+figure;
+plot(abs(M_1));
+figure;
+plot(abs(M_2));
+
+%% todos (for report)
+% 1. get phase offset (batch delay) for the mask recording vs outside:
+%   convert each freq stem to mag*e^jphi and subtract phi from non-masked
+%   fft stem. Show that this offset is similar across all freqs and so we
+%   discareded phase info.
+% 2. recording phrases and inverting mask tf and applying mask tf on them!
+% 3. distance recordings: 1 or 2. Show that not much noticeable diff in tf,
+%   so discarded idea.
 
 %% tf apply helper function
 
@@ -79,10 +95,10 @@ function scaled_mag = apply_tf(f, mag, tf, fmax, nbins)
     end
 end
 
-%% mic calibration and tf estimate helper function
+%% mic calibration helper function
 
-function [tf_mask, fr_bins] = get_mask_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib)
-    % calibrate mic 1 to mic 2
+function [mic_1_calib, mic_2_calib] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib)
+    % get mic recording ffts
     M_1 = fftshift(fft(mic_1));
     M_2 = fftshift(fft(mic_2));
     N_1 = length(M_1);
@@ -92,6 +108,7 @@ function [tf_mask, fr_bins] = get_mask_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fma
     fr_1  = -Fs_1/2:df_1:Fs_1/2-df_1;
     fr_2  = -Fs_2/2:df_2:Fs_2/2-df_2;
 
+    % indices/bounds for calibration and truncation
     idx_1 = fr_1(-fmax < fr_1 & fr_1 < fmax); idx_1 = round([(idx_1(1)-fr_1(1))/df_1 (idx_1(end)-fr_1(1))/df_1]);
     idx_2 = fr_2(-fmax < fr_2 & fr_2 < fmax); idx_2 = round([(idx_2(1)-fr_2(1))/df_2 (idx_2(end)-fr_2(1))/df_2]);
 
@@ -107,9 +124,16 @@ function [tf_mask, fr_bins] = get_mask_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fma
     M_2(idx_2(2)+1:end) = 0;
 
     % invert FFTs to time domain
-    mic_1_inv = ifft(ifftshift(M_1));
-    mic_2_inv = ifft(ifftshift(M_2));
+    mic_1_calib = ifft(ifftshift(M_1));
+    mic_2_calib = ifft(ifftshift(M_2));
+end
+
+%% mask tf estimate helper function (with calibration)
+
+function [tf_mask, fr_bins] = get_mask_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib)
+    % calibrate mic 1 to mic 2
+    [mic_1, mic_2] = calib_mics(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins, tf_calib);
 
     % get tf estimate for mic 1 and mic 2 (after calibration)
-    [tf_mask, fr_bins] = get_tf_estimate(mic_1_inv, mic_2_inv, Fs_1, Fs_2, fmax, nbins);
+    [tf_mask, fr_bins] = get_tf_estimate(mic_1, mic_2, Fs_1, Fs_2, fmax, nbins);
 end
